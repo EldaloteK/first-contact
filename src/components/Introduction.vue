699101f668853a96f989e218031f7fe23d584da7
Kristy Eley <kristyeley@KoraMarie.local>
2021-04-21 22:34:26 -0400

    first-contact: Creation of base files

---
+++ src/components/Introduction.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="portfolio">
+    TODO
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Introduction',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<style scoped>
+.portfolio {
+  display: grid;
+}
+</style>
